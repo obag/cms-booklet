366bca3e9edcaaa96b8bdd47e01525ff0e8b10ea
William Di Luigi <williamdiluigi@gmail.com>
2017-04-07 02:41:01 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ PACKAGE_DATA = {
 
 setup(
     name = 'cmsbooklet',
-    version = '0.7',
+    version = '0.8',
     description = 'cms-booklet: problem statement processor for CMS (Contest Management System)',
     author = 'Gabriele Farina',
     author_email = 'gabr.farina@gmail.com',
