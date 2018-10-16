b8f3cc6f7d2f7e1c16cff5cfb166ea1401181892
William Di Luigi <williamdiluigi@gmail.com>
2018-10-16 19:16:23 +0200

    release 1.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ PACKAGE_DATA = {
 
 setup(
     name='cmsbooklet',
-    version='1.0',
+    version='1.1',
     description='cmsbooklet: problem statement processor for CMS '
                 '(Contest Management System)',
     author='Gabriele Farina',
